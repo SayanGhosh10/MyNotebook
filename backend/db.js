82a87e9922a487d9f2486b151ab0f7c3f03f15c8
SayanGhosh10 <Sayanmyself50@gmail.com>
2023-12-28 00:13:44 +0530

    mongouri changed

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/inotebook?directConnection=true"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true"
 
 const connectTOMongo = () => {
     mongoose.connect(mongoURI);
